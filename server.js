a0553bcab9a57fdcd2a5028c1aa0f37539be7fba
Nitesh Vishwakarma <45270353+NiteshVishwakarma896@users.noreply.github.com>
2021-02-12 16:35:11 +0530

    Added formdata headers in server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "multipart/form-data,x-www-form-urlencoded,Origin, X-Requested-With, Content-Type, Accept, Authorization");
     if (req.method === 'OPTIONS') {
         res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
         res.status(200);
